6960c353125fca744ee22ea664dd22f28b5aca96
Dhiraj Borade <dhirajborade@gmail.com>
2017-12-01 14:28:26 -0500

    Testing Bitfield

--- src/BitfieldPayload.java
+++ src/BitfieldPayload.java
@@ -13,29 +13,11 @@ public class BitfieldPayload extends Payload {
 	 */
 	private static final long serialVersionUID = 8052696107658542567L;
 
-	private byte[] payload;
-
 	/**
 	 * @param bitfield
 	 */
 	public BitfieldPayload(byte[] payload) {
 		super();
-		this.payload = payload;
-	}
-
-	/**
-	 * @return the bitfield
-	 */
-	public byte[] getBitfield() {
-		return payload;
-	}
-
-	/**
-	 * @param bitfield
-	 *            the bitfield to set
-	 */
-	public void setBitfield(byte[] payload) {
-		this.payload = payload;
 	}
 
 	/**
