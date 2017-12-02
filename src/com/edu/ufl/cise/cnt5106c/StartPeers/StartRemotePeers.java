d4f42fb6514acb5a2562f2adb6caa916b8292ede
Dhiraj Borade <dhirajborade@gmail.com>
2017-12-01 19:22:04 -0500

    Final Commit

--- src/com/edu/ufl/cise/cnt5106c/StartPeers/StartRemotePeers.java
+++ src/com/edu/ufl/cise/cnt5106c/StartPeers/StartRemotePeers.java
@@ -56,20 +56,19 @@ public class StartRemotePeers {
 			myStart.getConfiguration(args);
 
 			// get current path
-			// String path = System.getProperty("user.dir");
+			String path = System.getProperty("user.dir");
 
 			// start clients at remote hosts
 			for (int i = 0; i < myStart.peerInfoVector.size(); i++) {
 				RemotePeerInfo pInfo = (RemotePeerInfo) myStart.peerInfoVector.elementAt(i);
 				System.out.println("Start remote peer " + pInfo.getPeerID() + " at " + pInfo.getPeerIP());
-				Runtime.getRuntime().exec(
-						"ssh " + pInfo.getPeerIP() + " cd " + pInfo.getProjectPath() + "; javac PeerProcess.java");
+				Runtime.getRuntime().exec("ssh " + pInfo.getPeerIP() + " cd " + path + "; javac PeerProcess.java");
 			}
 			for (int i = 0; i < myStart.peerInfoVector.size(); i++) {
 				RemotePeerInfo pInfo = (RemotePeerInfo) myStart.peerInfoVector.elementAt(i);
 				System.out.println("Start remote peer " + pInfo.getPeerID() + " at " + pInfo.getPeerIP());
-				Runtime.getRuntime().exec("ssh " + pInfo.getPeerIP() + " cd " + pInfo.getProjectPath()
-						+ "; java PeerProcess " + pInfo.getPeerID());
+				Runtime.getRuntime()
+						.exec("ssh " + pInfo.getPeerIP() + " cd " + path + "; java PeerProcess " + pInfo.getPeerID());
 			}
 			System.out.println("Starting all remote peers has done.");
 
